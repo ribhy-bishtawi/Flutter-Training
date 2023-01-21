8a911d0b361db63335a70e8d6c3b53d9c5ee015d
ribhy-bishtawi <ribhy2001@gmail.com>
2023-01-21 23:24:45 +0300

    Coontact Page

--- contact/lib/main.dart
+++ contact/lib/main.dart
@@ -1,115 +1,288 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ContactPage());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+enum APP_THEME { LIGHT, DARK }
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+class MyAppThemes {
+  //Method to provide light theme
+  static ThemeData appThemeLight() {
+    return ThemeData(
+      // Define the default brightness and colors for the overall app.
+      brightness: Brightness.light,
+
+      //Theme for app bar
+      appBarTheme: const AppBarTheme(
+        //AppBar's color
+        color: Colors.green,
+        //Theme for AppBar's icons
+        iconTheme: IconThemeData(
+          //Dark color icons on light colored background
+          color: Colors.black,
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+
+      //Theme for app's icons
+      iconTheme: IconThemeData(
+        color: Colors.indigo.shade800,
+      ),
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        //White background
+        backgroundColor: Colors.white,
+
+        //Black plus (+) sign for FAB
+        foregroundColor: Colors.black,
+      ),
+    );
+  }
+
+  static ThemeData appThemeDark() {
+    return ThemeData(
+      // Define the default brightness and colors for the overall app.
+      brightness: Brightness.dark,
+
+      //Theme for app bar
+      appBarTheme: const AppBarTheme(
+        //AppBar's background color is dark this time
+        color: Colors.black,
+
+        //Light color for the app bar's icons
+        iconTheme: IconThemeData(
+          color: Colors.white,
+        ),
+      ),
+
+      //App's icons are colored in orange color
+      iconTheme: const IconThemeData(
+        color: Colors.orange,
+      ),
+      floatingActionButtonTheme: const FloatingActionButtonThemeData(
+        //dark background for FAB
+        backgroundColor: Colors.black,
+
+        //White plus (+) sign for FAB
+        foregroundColor: Colors.white,
+      ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ContactPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ContactPage> createState() => _ContactPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _ContactPageState extends State<ContactPage> {
+  var currentTheme = APP_THEME.LIGHT;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return MaterialApp(
+      theme: currentTheme == APP_THEME.DARK
+          ? MyAppThemes.appThemeDark()
+          : MyAppThemes.appThemeLight(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: buildAppBarWidget(),
+        body: buildBody(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: IconButton(
+            icon: Icon(Icons.threesixty),
+            onPressed: () {
+              setState(() {
+                //NEW CODE: Currently selected theme toggles when FAB is pressed
+                currentTheme == APP_THEME.DARK
+                    ? currentTheme = APP_THEME.LIGHT
+                    : currentTheme = APP_THEME.DARK;
+              });
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  PreferredSizeWidget buildAppBarWidget() {
+    return AppBar(
+      leading: const Icon(
+        Icons.arrow_back,
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.star_border),
+          onPressed: () {
+            print("Contact is starred");
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget buildBody() {
+    return ListView(
+      children: [
+        Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: 250,
+              child: Image.network(
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Emojione_1F60E.svg/640px-Emojione_1F60E.svg.png",
+                //Make picture fit its parent
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 60,
+              child: Row(
+                children: const [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Text("Ribhi Bishtawi",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold)),
+                  ),
+                ],
+              ),
+            ),
+            const Divider(
+              color: Colors.black,
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 8, bottom: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  buildCallButton(),
+                  buildTextButton(),
+                  buildVideoCallButton(),
+                  buildEmailButton(),
+                  buildDirectionsButton(),
+                  buildPayButton(),
+                ],
+              ),
+            ),
+            const Divider(
+              color: Colors.grey,
+            ),
+
+            mobilePhoneListTile("05525587511", "Mobile"),
+            mobilePhoneListTile("0592751716", "Other"),
+            //NEW CODE: Adding `Divider` widget with grey color
+            const Divider(
+              color: Colors.grey,
+            ),
+            contactAddresses(Icons.email, "ribhy2001@gmail.com", "Email"),
+            contactAddresses(Icons.location_on, "Ankara", "Home"),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget buildCallButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: const Icon(
+            Icons.call,
+          ),
+          onPressed: () {},
+        ),
+        const Text("Call"),
+      ],
+    );
+  }
+
+  Widget buildTextButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.message,
+          ),
+          onPressed: () {},
+        ),
+        const Text("Text"),
+      ],
+    );
+  }
+
+  buildDirectionsButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.directions,
+          ),
+          onPressed: () {},
+        ),
+        const Text("Directions"),
+      ],
+    );
+  }
+
+  buildVideoCallButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.video_call,
+          ),
+          onPressed: () {},
+        ),
+        const Text("Video Call"),
+      ],
+    );
+  }
+
+  buildEmailButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.email,
+          ),
+          onPressed: () {},
+        ),
+        const Text("Email"),
+      ],
+    );
+  }
+
+  buildPayButton() {
+    return Column(
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            Icons.attach_money,
+          ),
+          onPressed: () {},
+        ),
+        const Text("Pay"),
+      ],
+    );
+  }
+
+  Widget mobilePhoneListTile(dynamic phone, dynamic type) {
+    return ListTile(
+      leading: const Icon(Icons.call),
+      title: Text(phone),
+      subtitle: Text(type),
+      trailing: IconButton(
+        icon: const Icon(Icons.message),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  Widget contactAddresses(dynamic icon, dynamic email, dynamic type) {
+    return ListTile(
+      leading: Icon(icon),
+      title: Text(email),
+      subtitle: Text(type),
     );
   }
 }
